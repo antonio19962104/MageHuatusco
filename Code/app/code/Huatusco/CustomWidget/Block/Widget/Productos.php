19850630093719ce6668af71e1fba5bd6f652c34
j.antoniomg96@outlook.com <j.antoniomg96@outlook.com>
2021-05-28 15:55:36 -0500

    primer widget data statica

--- Code/app/code/Huatusco/CustomWidget/Block/Widget/Productos.php
+++ Code/app/code/Huatusco/CustomWidget/Block/Widget/Productos.php
@@ -1,11 +1,15 @@
 <?php 
-namespace Mageplaza\HelloWorld\Block\Widget;
+namespace Huatusco\CustomWidget\Block\Widget;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface; 
  
-class Posts extends Template implements BlockInterface {
-
+class Productos extends Template implements BlockInterface {
 	protected $_template = "widget/productos.phtml";
-
+	public function getproducts(){
+		$listproductos = [];
+		array_push($listproductos, "producto 1");
+		array_push($listproductos, "producto 2");
+		return $listproductos;
+	}
 }
