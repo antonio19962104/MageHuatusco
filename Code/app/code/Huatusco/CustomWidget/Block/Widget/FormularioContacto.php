805f95bc9e73d9779957a37b2c809f85fe9b008c
j.antoniomg96@outlook.com <j.antoniomg96@outlook.com>
2021-05-29 11:32:34 -0500

    2do widget

---
+++ Code/app/code/Huatusco/CustomWidget/Block/Widget/FormularioContacto.php
@@ -0,0 +1,9 @@
+<?php
+namespace Huatusco\CustomWidget\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface; 
+
+class FormularioContacto extends Template implements BlockInterface{
+    protected $_template = "widget/FormularioContacto.phtml";
+}
