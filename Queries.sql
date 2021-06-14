0b6f65a779e56e2466b2d09bd3f86967d0b53e8d
jamurillo <jamurillo@grupoautofin.com>
2021-06-14 17:29:40 -0500

    s

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,11 @@
 use RH_Des
-select * from Reporte
-select * from EncuestaReporte
+SELECT * FROM DEMO WHERE EntidadId = 9 AND Usuario = 'jamurillo@grupoautofin.com'
+
+-- capacitacioncorpo@grupoautofin.com
+-- demo@grupoautofin.com
+-- dva.avaldes@grupoautofin.com
+-- dva.ocamacho@grupoautofin.com
+-- jamurillo
+-- jamurillo@grupoautofin.com
+-- sfdk
+GetByUnidad
