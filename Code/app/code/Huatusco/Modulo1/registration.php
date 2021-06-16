8627069d318f340b711223be1a99ce27a4751c4e
jamurillo <jamurillo@grupoautofin.com>
2021-06-16 15:31:30 -0500

    Elimine modulo

--- Code/app/code/Huatusco/Modulo1/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Huatusco_Modulo1',
-	__DIR__
-);
--- Code/app/code/Huatusco/Productos/Block/Index.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Huatusco\Productos\Block;
-class Index extends \Magento\Framework\View\Element\Template
-{
-
-}
