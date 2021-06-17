991a3b635aaef936930cacba5dd2147688b7856d
jamurillo <jamurillo@grupoautofin.com>
2021-06-17 15:03:44 -0500

    Retornando vista

--- Code/app/code/Huatusco/Men/Block/Index.php
+++ Code/app/code/Huatusco/Men/Block/Index.php
@@ -2,5 +2,8 @@
 namespace Huatusco\Men\Block;
 class Index extends \Magento\Framework\View\Element\Template
 {
-
+  public function getHelloWorldTxt()
+  {
+      return 'Hello world!';
+  }
 }
--- Code/app/code/Huatusco/Men/Controller/Index/Index.php
+++ Code/app/code/Huatusco/Men/Controller/Index/Index.php
@@ -4,25 +4,38 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     protected $resultJsonFactory;
     protected $_pageFactory;
+    protected $_resultPageFactory;
     public function __construct(
        \Magento\Framework\App\Action\Context $context,
        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
-       \Magento\Framework\View\Result\PageFactory $pageFactory)
+       \Magento\Framework\View\Result\PageFactory $pageFactory,
+       \Magento\Framework\View\Result\PageFactory $resultPageFactory)
     {
        $this->resultJsonFactory = $resultJsonFactory;
        $this->_pageFactory = $pageFactory;
+       $this->_resultPageFactory = $resultPageFactory;
        parent::__construct($context);
     }
     public function execute()
     {
-       /*
-       return json
-       $result = $this->resultJsonFactory->create();
+       /*return json*/
+       /*$result = $this->resultJsonFactory->create();
        $data = ['message' => 'Hello world!'];
        return $result->setData($data['message']);*/
 
        /*return view*/
-       return $this->_pageFactory->create();
+       // return $this->_pageFactory->create();
+
+       /**/
+       $resultPage = $this->_resultPageFactory->create();
+       $resultPage->getConfig()->getTitle()->prepend(__(' heading '));
+
+       $block = $resultPage->getLayout()
+                ->createBlock('Huatusco\Men\Block\Index')
+                ->setTemplate('Huatusco_Men::index.phtml')
+                ->toHtml();
+        $this->getResponse()->setBody($block);
 
      }
 }
+// https://magento.stackexchange.com/questions/232177/magento2-how-to-call-template-from-a-controller/232182
